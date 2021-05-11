12e94a87dec37a503766f4093bbba8e2b870d7ce
rjmcbain <rmcbain01@gmail.com>
2021-05-11 16:31:28 -0700

    fixing box

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -14,7 +14,7 @@
     </div>
     <!-- Box 2 -->
     <div class="shadow-md bg-blue-200 p-10 text-center rounded">
-      <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
       <div class="text-2xl mb-4">
         <span class="font-bold">New:</span>
         {{ numberWithCommas(stats.NewDeaths) }}
