ea7bdf373e913be9b672f92380aa2f7e113c36ed
rjmcbain <rmcbain01@gmail.com>
2021-05-11 16:27:33 -0700

    deploying to netlify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,15 @@
 <template>
   <main v-if="!loading">
-    <DataTitle :test="title" :dataDate="dataDate" />
+    <DataTitle :text="title" :dataDate="dataDate" />
     <DataBoxes :stats="stats" />
-    <CountrySelect :countries="countries" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+    <button
+      @click="clearCountryData"
+      v-if="stats.Country"
+      class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+    >
+      Clear Country
+    </button>
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -34,6 +41,17 @@ export default {
       const res = await fetch("https://api.covid19api.com/summary");
       const data = await res.json();
       return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = "Global";
+      this.stats = data.Global;
+      this.loading = false;
     },
   },
   async created() {
