ce97fea479af511cffea83381bc21b708c4395cf
rjmcbain <rmcbain01@gmail.com>
2021-05-11 13:19:06 -0700

    adding components and api

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,17 @@
+<template>
+  <select v-model="selected">
+    <option value="0">Select Country</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "CountrySelect",
+  props: ["countries"],
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,47 @@
 <template>
-  <div class="home">
-    Hello World
-  </div>
+  <main v-if="!loading">
+    <DataTitle :test="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+    <CountrySelect :countries="countries" />
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="Loading Image" />
+  </main>
 </template>
 
 <script>
+import DataTitle from "@/components/DataTitle";
+import DataBoxes from "@/components/DataBoxes";
+import CountrySelect from "@/components/CountrySelect";
 export default {
   name: "Home",
-  components: {},
+  components: { DataTitle, DataBoxes, CountrySelect },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
 };
 </script>
